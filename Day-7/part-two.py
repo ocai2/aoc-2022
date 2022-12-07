7483135bff21ec84703e8deac1a1ef74df33b198
ocai2 <osborn.cai@gmail.com>
2022-12-07 20:15:35 +1100

    day 7

---
+++ Day-7/part-two.py
@@ -0,0 +1,58 @@
+class directory:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.size = 0
+        self.children = []
+
+def main():
+    with open('day-7\input.txt', 'r') as f:
+        data = f.read().splitlines()
+
+    root = directory('/', None)
+    current_directory = root
+    
+    for i in range(1, len(data), 1):
+        line = data[i].split()
+        if line[0] == "$":
+            # cd
+            if len(line) > 2:
+                if line[2] == "..":
+                    current_directory = current_directory.parent
+                else:
+                    current_directory = next(d for d in current_directory.children if d.name == line[2])
+            # ls
+            else:
+                i += 1
+                size = 0
+                while data[i][0] != "$":
+                    line = data[i].split()
+                    if line[0] == "dir":
+                        current_directory.children.append(directory(line[1], current_directory))
+                    else:
+                        size += int(line[0])
+                    i += 1
+                    if i >= len(data):
+                        break
+                current_directory.size += size
+                add_to_parent(current_directory, size)
+    
+    q = [root]
+    smallest_but_big_enough_directory_size = 70000000
+    space_needed = 30000000 - (70000000 - root.size)
+    while len(q) > 0:
+        d = q.pop()
+        if d.size >= space_needed and d.size < smallest_but_big_enough_directory_size:
+            smallest_but_big_enough_directory_size = d.size
+        for c in d.children:
+            q.append(c)
+    
+    print(smallest_but_big_enough_directory_size)
+                
+def add_to_parent(x, n):
+    while x.parent != None:
+        x = x.parent
+        x.size += n
+
+if __name__ == "__main__":
+    main()
